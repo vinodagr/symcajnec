59cd9a69e9d37d18473383579e5d0f0510ee0e80
vinodagr <111001042+vinodagr@users.noreply.github.com>
2026-07-17 16:59:11 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo
   {
     public static void main(String args[])
     {
-     System.out.println("mgm university chh.sambhajinagar............");
+     System.out.println("mgm university ");
     }
 
 
