f5c7e906306617094ada5ed130ccc608243111f7
vinodagr <111001042+vinodagr@users.noreply.github.com>
2026-07-30 15:49:09 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo
   {
     public static void main(String args[])
     {
-     System.out.println("mgm jnec mca dept CSN...");
+     System.out.println("mgm jnec mca dept CSN... gegere  r r rurr ");
     }
 
 
